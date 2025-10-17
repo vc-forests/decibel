b9409f7c4f0eb504ffc30b1504e6d485d129f6c7
Bao Tran <nguyenbao1003@hotmail.com>
2025-10-16 22:28:48 -0400

    process audio from microphone

---
+++ recording-processor.js
@@ -0,0 +1,36 @@
+class RecordingProcessor extends AudioWorkletNode {
+    constructor(options) {
+        super();
+        this.sampleRate = 0;
+        this.maxRecordingFrames = 0;
+        this.numberOfChannels = 0;
+
+        if (options && options.processorOptions) {
+            const {
+                numberOfChannels,
+                sampleRate,
+                maxFrameCount,
+            } = options.processorOptions;
+
+            this.sampleRate = sampleRate;
+            this.maxRecordingFrames = maxFrameCount;
+            this.numberOfChannels = numberOfChannels;
+        }
+    }
+
+    process(inputs, outputs, params) {
+        console.log('input', input);
+        for (let input = 0; input < 1; input++) {
+            // channel < inputs[input].length
+            for (let channel = 0; channel < this.numberOfChannels; channel++) {
+                for (let sample = 0; sample < inputs[input][channel].length; sample++) {
+                    // Pass data directly to output, unchanged.
+                    outputs[input][channel][sample] = currentSample;
+                }
+            }
+        }
+        return true;
+    }
+}
+
+registerProcessor('recording-processor', RecordingProcessor);
