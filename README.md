b731ff23142f866d661a2330cdf90116802182d4
n1340t <nguyenbao1003@hotmail.com>
2025-10-26 18:26:17 -0400

    bug fixed: correct class name in recording module

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # decibel
+
+### Good tutorials
+- https://web.dev/patterns/media/microphone-process?hl=en
+- https://github.com/GoogleChromeLabs/web-audio-samples/tree/main/src/audio-worklet/migration/worklet-recorder
--- recording-processor.js
+++ recording-processor.js
@@ -1,4 +1,4 @@
-class RecordingProcessor extends AudioWorkletNode {
+class RecordingProcessor extends AudioWorkletProcessor {
     constructor(options) {
         super();
         this.sampleRate = 0;
@@ -19,11 +19,12 @@ class RecordingProcessor extends AudioWorkletNode {
     }
 
     process(inputs, outputs, params) {
-        console.log('input', input);
+        console.log('input', inputs);
         for (let input = 0; input < 1; input++) {
             // channel < inputs[input].length
             for (let channel = 0; channel < this.numberOfChannels; channel++) {
                 for (let sample = 0; sample < inputs[input][channel].length; sample++) {
+                    const currentSample = inputs[input][channel][sample];
                     // Pass data directly to output, unchanged.
                     outputs[input][channel][sample] = currentSample;
                 }
